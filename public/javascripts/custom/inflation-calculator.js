1cd5a106df566a8d0abb3c04e7ee3b523723e891
AnnaErshova <anna.ershova@gmail.com>
2015-07-21 17:08:11 -0400

    all done

--- public/javascripts/custom/inflation-calculator.js
+++ public/javascripts/custom/inflation-calculator.js
@@ -1,1 +1,26 @@
-// code your functions here
+function fetchOption() {
+  return {
+    start: $('#startDate').val(),
+    end:   $('#endDate').val(),
+    amount:$('#startPrice').val()
+  }
+}
+
+function fetchEndPrice(callback) {
+  // send AJAX request and get information back
+  var options = fetchOptions();
+  var apiURL = 'http://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?';
+  $.getJSON(apiURL, {
+    country: 'united-states',
+    start: options.start,
+    end: options.end,
+    amount: options.amount,
+    format: true
+  }).done(callback)
+}
+
+function addPriceToPage(price){
+  var finalPrice = price.substring(1);
+  $('#endPrice').val(finalPrice);
+  return price;
+}
--- public/javascripts/custom/on-click.js
+++ public/javascripts/custom/on-click.js
@@ -1,7 +1,9 @@
 "use strict";
 
-$(document).ready(function() {
-
+$(document).ready(function(){
   // add your on click event handler here
-  
+  // equivalent of runner
+  $('#calc').on('click', function(){
+    fetchEndPrice(addPriceToPage())
+  });
 });
